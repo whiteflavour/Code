f2137c2b85e4448b48083980dbdcc1393e380dd9
whiteflavour <1311798851@qq.com>
2022-02-02 16:36:27 +0800

    updated design pattern

--- LeetCodeDemo/leetcode/src/main/java/com/google/Solution.java
+++ LeetCodeDemo/leetcode/src/main/java/com/google/Solution.java
@@ -3,22 +3,59 @@ package com.google;
 import java.util.*;
 
 public class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int ans = Integer.MAX_VALUE;
-        int left, right;
-        left = right = 0;
-        while (right <= nums.length)
-            if (target > 0) {
-                if (right == nums.length) {
-                    return ans;
+    private Trie root;
+
+    public WordDictionary() {
+        root = new Trie();
+    }
+
+    public void addWord(String word) {
+        root.insert(word);
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private class Trie {
+        private Trie[] children;
+        private boolean isEnd;
+
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Trie node = this;
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
                 }
-                target -= nums[right];
-                ++right;
-            } else {
-                ans = Math.min(ans, right - left);
-                target += nums[left];
-                ++left;
+                node = node.children[index];
             }
-        return ans;
+            node.isEnd = true;
+        }
+    }
+
+    public boolean dfs(String word, int index, Trie node) {
+        if (index == word.length()) {
+            return node.isEnd;
+        }
+        char ch = word.charAt(index);
+        if (Character.isLetter(ch)) {
+            int childIndex = ch - 'a';
+            Trie child = node.children[childIndex];
+            return child != null && dfs(word, index + 1, child);
+        } else {
+            for (int i = 0; i < 26; ++i) {
+                Trie child = node.children[i];
+                if (child != null && dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
