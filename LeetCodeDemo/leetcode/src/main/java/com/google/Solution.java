bd486adb8f0fd6cb1bc91f9e83d227ac212c33ee
whiteflavour <1311798851@qq.com>
2022-02-17 16:26:35 +0800

    updated leetcode

--- LeetCodeDemo/leetcode/src/main/java/com/google/Solution.java
+++ LeetCodeDemo/leetcode/src/main/java/com/google/Solution.java
@@ -3,67 +3,29 @@ package com.google;
 import java.util.*;
 
 public class Solution {
-    private Set<String> ans;
-    private int[][] dirs;
+    private static final int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private List<List<Integer>> ans = new ArrayList<>();
 
-    public List<String> findWords(char[][] board, String[] words) {
-        ans = new HashSet<>();
-        dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        Trie root = new Trie();
-        for (String word : words) {
-            root.insert(word);
-        }
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                dfs(board, root, i, j, ans);
-            }
-        }
-        return new ArrayList<>(ans);
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        backtrack(new ArrayList<>(), k, n, 0);
+        return ans;
     }
 
-    public void dfs(char[][] board, Trie node, int row, int col, Set<String> ans) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+    public void backtrack(List<Integer> combine, int k, int n, int start) {
+        if (k == 0 && n == 0) {
+            ans.add(new ArrayList<>(combine));
             return;
         }
-        char ch = board[row][col];
-        if (!node.children.containsKey(ch)) {
+        if (k == 0 || n < 0) {
             return;
         }
-        Trie next = node.children.get(ch);
-        if (!"".equals(next.word)) {
-            ans.add(next.word);
-        }
-        if (!next.children.isEmpty()) {
-            board[row][col] = '#';
-            for (int[] dir : dirs) {
-                int i = row + dir[0], j = col + dir[1];
-                dfs(board, next, i, j, ans);
+        for (int i = start; i < nums.length; ++i) {
+            if (!combine.isEmpty() && nums[i] < combine.get(combine.size() - 1)){
+                continue;
             }
-            board[row][col] = ch;
-        } else {
-            node.children.remove(ch);
-        }
-    }
-
-    class Trie {
-        private String word;
-        private Map<Character, Trie> children;
-
-        public Trie() {
-            word = "";
-            children = new HashMap<>();
-        }
-
-        public void insert(String word) {
-            Trie node = this;
-            for (int i = 0; i < word.length(); ++i) {
-                char ch = word.charAt(i);
-                if (!node.children.containsKey(ch)) {
-                    node.children.put(ch, new Trie());
-                }
-                node = node.children.get(ch);
-            }
-            node.word = word;
+            combine.add(nums[i]);
+            backtrack(combine, k - 1, n - nums[i], start + 1);
+            combine.remove(combine.size() - 1);
         }
     }
 }
