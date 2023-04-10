ecb7e7351164a91c13159799dbbb932e884f0dd4
whiteflavour <61577204+whiteflavour@users.noreply.github.com>
2023-04-10 22:23:38 +0800

    Delete Java/Spring/GitDemos/shop_springboot directory

--- Java/Spring/GitDemos/shop_springboot/src/main/java/com/google/mapper/HostHolderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.google.mapper;
-
-import com.google.entity.Consumer;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HostHolderMapper {
-    Consumer find(int id);
-}
--- Java/Spring/GitDemos/shop_springboot/src/main/java/com/google/service/IHostHolderService.java
+++
@@ -1,7 +0,0 @@
-package com.google.service;
-
-import com.google.entity.Consumer;
-
-public interface IHostHolderService {
-    Consumer getConsumer(int id);
-}
